854c92829ac1a8c5228fee3428131ccf83b921f7
Stefan Walluhn <stefan@walluhn.de>
2024-11-26 23:13:14 +0100

    hard-wire tuxracer mapping

--- src/mapper.py
+++ src/mapper.py
@@ -2,12 +2,14 @@ from evdev import ecodes
 
 
 class Mapper:
+    # taken from https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
     mapping = {
-        "0": ecodes.KEY_KP2,
-        "1": ecodes.KEY_A,
-        "2": ecodes.KEY_B,
-        "3": ecodes.KEY_C,
-        "#": ecodes.KEY_D,
+        "0": ecodes.KEY_T,
+        "2": ecodes.KEY_UP,
+        "4": ecodes.KEY_LEFT,
+        "5": ecodes.KEY_SPACE,
+        "6": ecodes.KEY_RIGHT,
+        "8": ecodes.KEY_DOWN,
     }
 
     def keycode_from_number(self, number):
